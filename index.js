b378eba3c033180ee43273464b9276940c081aca
kevinz217 <kevinz217@nycstudents.net>
2024-12-12 01:48:00 -0500

    Yes

--- index.js
+++ index.js
@@ -10,14 +10,38 @@ async function getComment() {
     return j;
 }
 
+async function getPost() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/')
+    let j = await response.json()
+    return j;
+}
+
+
 getUser()
     .then((json) => {
-    address1 = document.getElementById('address1')
-    address1.innerHTML = JSON.stringify(json.address)
+        address = document.getElementById('address-div')
+        for (let i = 0; i < 3; i++ ) {
+            address.innerHTML += JSON.stringify(json[i].address)
+            address.innerHTML += "<br />"
+        }
+
+        users = document.getElementById('user-div')
+        for (let i = 0; i < 3; i++ ) {
+            users.innerHTML += JSON.stringify(json[i])
+            users.innerHTML += "<br />"
+        }
 })
 
 getComment() 
     .then((json) => {
     comment1 = document.getElementById('comment1')
-    comment1.innerHTML = json.body
+    comment1.innerHTML += json.body
+})
+
+getPost()
+    .then((json) => {
+        post1 = document.getElementById('post1')
+        post1.innerHTML += json[0].body
+        posts = document.getElementById('post-div')
+        posts.innerHTML += json[1].body
 })
